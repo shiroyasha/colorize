3c8529698750dbd098f06952770cae20e2e302c8
Igor Šarčević <igor@renderedtext.com>
2015-12-29 22:08:29 +0100

    Fill project info

--- mix.exs
+++ mix.exs
@@ -3,29 +3,27 @@ defmodule Colorize.Mixfile do
 
   def project do
     [app: :colorize,
-     version: "0.0.1",
+     version: "0.1.0",
+     description: "Colorize your text in the console",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  def package do
+    [
+      maintainers: ["Igor Šarčević (shiroyasha)"],
+      links: %{"GitHub": "https://github.com/shiroyasha/colorize"},
+      licenses: ["MIT"]
+    ]
+  end
+
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
